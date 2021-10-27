c146242d04e7cd9857f1968a2696c3ea610754a4
Szymon Pojda <szymonpojda@gmail.com>
2021-10-27 01:05:02 +0100

    wip

--- src/js/script.js
+++ src/js/script.js
@@ -83,25 +83,42 @@
 
     }
 
+    getElements() {
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion() {
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      console.log(clickableTrigger);
+      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      //console.log(clickableTrigger);
+
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function (event) {
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
+
         /* prevent default action for event */
         event.preventDefault();
+
         /* find active product (product that has active class) */
         const activeProduct = document.querySelectorAll(select.all.menuProductsActive);
+        
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         for (let active of activeProduct) {
+
           if (active !== thisProduct.element) {
+
             active.classList.remove(classNames.menuProduct.wrapperActive);
           }
         }
         /* toggle active class on thisProduct.element */
+
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
 
