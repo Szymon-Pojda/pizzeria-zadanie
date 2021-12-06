93578e319eb898c22e71116939df155608842955
Szymon Pojda <szymonpojda@gmail.com>
2021-12-06 09:53:22 +0000

    fix 11.4

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import { settings, select, classNames, templates } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
-import Home from './component/Home.js';
+import Home from './components/Home.js';
 
 const app = {
   initPages: function () {
